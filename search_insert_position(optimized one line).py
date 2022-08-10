4eaddcedf7bf7dc744316219dd56c2e75a3a4339
samyogita <77333607+samyogita@users.noreply.github.com>
2022-08-11 00:26:21 +0530

    Search Insert Position(one line solution)
    Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
    
    You must write an algorithm with O(log n) runtime complexity.

---
+++ search_insert_position(optimized one line).py
@@ -0,0 +1,8 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        return bisect_left(nums, target)
+                    
+               
+            
+            
+            
