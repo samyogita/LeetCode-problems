ad6463d0624306534db4937b8f7a36fe185137c5
samyogita <77333607+samyogita@users.noreply.github.com>
2022-08-12 00:16:25 +0530

    N-th Tribonacci Number
    The Tribonacci sequence Tn is defined as follows: 
    
    T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
    
    Given n, return the value of Tn.

---
+++ tribonacci_memoization.py
@@ -0,0 +1,15 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        # Tabulation
+        dp = [-1] * (n + 1)
+        def fib(n):
+            if n == 0 or n == 1:
+                return n
+            elif n == 2:
+                return 1
+            
+            if dp[n] != -1:
+                return dp[n]
+            dp[n] = fib(n-1) + fib(n-2) + fib(n-3)
+            return dp[n]
+        return fib(n)
