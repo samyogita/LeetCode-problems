1c03b29350b2fb0448c9c6a48f7b20bae47317fa
samyogita <77333607+samyogita@users.noreply.github.com>
2023-04-14 22:13:13 +0545

    Update LPS.py
    Given a string s, find the longest palindromic subsequence's length in s.
    
    A subsequence is a sequence that can be derived from another sequence by deleting some or no elements without changing the order of the remaining elements.

--- LPS.py
+++ LPS.py
@@ -1,20 +1,10 @@
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
-        dp = [[-1 for _ in range(len(s))] for _ in range(len(s))]
-        def lps(s, l, r, dp):
+        @cache
+        def lps(l, r):
             if l >= r:
-                if l == r:
-                    return 1
-                else:
-                    return 0
-            if dp[l][r] != -1:
-                return dp[l][r]
-            
+                return int(l == r)
             if s[l] == s[r]:
-                dp[l][r] = 2 + lps(s, l+1, r-1, dp)
-            else:
-                dp[l][r] =  max(lps(s, l, r-1, dp), lps(s, l+1, r, dp))
-            
-            return dp[l][r]
-        return lps(s, 0, len(s)-1, dp)
-    
+                return 2 + lps(l+1, r-1)
+            return max(lps(l+1,r), lps(l, r-1))
+        return lps(0, len(s)-1)
