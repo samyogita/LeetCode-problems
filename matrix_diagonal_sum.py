adcd2b50ada51d9d6df70f712f8270687dfbc451
samyogita <77333607+samyogita@users.noreply.github.com>
2023-05-08 20:33:47 +0545

    Matrix Diagonal Sum
    Given a square matrix mat, return the sum of the matrix diagonals.
    
    Only include the sum of all the elements on the primary diagonal and all the elements on the secondary diagonal that are not part of the primary diagonal.

---
+++ matrix_diagonal_sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        res = 0
+        n = len(mat)
+        for i in range(n):
+            res += mat[i][i] + mat[i][n-i-1]
+        if n % 2 == 0:
+            return res 
+        return res - mat[n//2][n//2]
