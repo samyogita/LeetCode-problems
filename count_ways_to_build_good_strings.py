28d6caad3e319203118610cad60f425a49c3bb96
samyogita <77333607+samyogita@users.noreply.github.com>
2023-05-13 21:31:08 +0545

    Count Ways To Build Good Strings
    Given the integers zero, one, low, and high, we can construct a string by starting with an empty string, and then at each step perform either of the following:
    
    Append the character '0' zero times.
    Append the character '1' one times.
    This can be performed any number of times.
    
    A good string is a string constructed by the above process having a length between low and high (inclusive).
    
    Return the number of different good strings that can be constructed satisfying these properties. Since the answer can be large, return it modulo 10^9 + 7.

--- count_ways_to_build_good_strings.py
+++ count_ways_to_build_good_strings.py
@@ -1,13 +1,14 @@
 class Solution:
     def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
-        dp = [1] + [0] * high
-        for i in range(1, high+1):
-            if i >= zero:
-                dp[i] += dp[i - zero]
-            if i >= one:
-                dp[i] += dp[i - one]
+        MOD = 10**9+7
         res = 0
-        for i in range(low, high+1):
-            res += dp[i]
-        return res % int(1e9 + 7)
+        @cache
+        def dp(digits):
+            if digits > high:
+                return 0
+            res = (digits>=low)
+            res += dp(digits+zero)
+            res += dp(digits+one)
+            return res % MOD
+        return dp(0)
         
