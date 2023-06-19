902365eedefa5b509ce33c787c057667d8849228
samyogita <77333607+samyogita@users.noreply.github.com>
2023-06-19 10:06:25 +0545

    Find the Highest Altitude
    There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes. The biker starts his trip on point 0 with altitude equal 0.
    
    You are given an integer array gain of length n where gain[i] is the net gain in altitude between points i​​​​​​ and i + 1 for all (0 <= i < n). Return the highest altitude of a point.

---
+++ find_the_highest_altitude.py
@@ -0,0 +1,7 @@
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        res = [0]
+        for i in gain:
+            res.append((res[-1]+i))
+        return max(res)
+        
