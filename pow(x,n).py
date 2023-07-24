129205d28275ebdd4558d5a66757c0a52936a9a0
samyogita <77333607+samyogita@users.noreply.github.com>
2023-07-24 22:40:25 +0530

    Pow(x,n)
    Implement pow(x, n), which calculates x raised to the power n (i.e., x^n).

---
+++ pow(x,n).py
@@ -0,0 +1,18 @@
+class Solution:
+    def binaryExp(self, x: float, n: int) -> float:
+        
+        if n == 0:
+            return 1
+      
+        if n < 0:
+            return 1.0 / self.binaryExp(x, -1 * n)
+       
+        
+        if n % 2 == 1:
+            return x * self.binaryExp(x * x, (n - 1) // 2)
+        
+        else:
+            return self.binaryExp(x * x, n // 2)
+
+    def myPow(self, x: float, n: int) -> float:
+        return self.binaryExp(x, n)
