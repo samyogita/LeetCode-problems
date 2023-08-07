f2b021451da313dd2899a749f6a7a159f6c8620c
samyogita <77333607+samyogita@users.noreply.github.com>
2023-08-07 22:10:31 +0530

    Search a 2D Matrix
    ou are given an m x n integer matrix matrix with the following two properties:
    
    Each row is sorted in non-decreasing order.
    The first integer of each row is greater than the last integer of the previous row.
    Given an integer target, return true if target is in matrix or false otherwise.

---
+++ search_a_2D_matrix.py
@@ -0,0 +1,19 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix:
+            return False
+        m, n = len(matrix), len(matrix[0])
+        l, r = 0, m * n - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+            mid_row, mid_col = divmod(mid, n)
+
+            if matrix[mid_row][mid_col] == target:
+                return True
+            elif matrix[mid_row][mid_col] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+
+        return False
