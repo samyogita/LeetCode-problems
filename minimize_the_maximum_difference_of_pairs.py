dc89edec6c887b438b4b9def9126e691f38cdcc1
samyogita <77333607+samyogita@users.noreply.github.com>
2023-08-09 20:46:20 +0530

    Minimize the Maximum Difference of Pairs
    You are given a 0-indexed integer array nums and an integer p. Find p pairs of indices of nums such that the maximum difference amongst all the pairs is minimized. Also, ensure no index appears more than once amongst the p pairs.
    
    Note that for a pair of elements at the index i and j, the difference of this pair is |nums[i] - nums[j]|, where |x| represents the absolute value of x.
    
    Return the minimum maximum difference among all p pairs. We define the maximum of an empty set to be zero.

--- minimize_the_maximum_difference_of_pairs.py
+++ minimize_the_maximum_difference_of_pairs.py
@@ -1,25 +1,23 @@
 class Solution:
     def minimizeMax(self, nums: List[int], p: int) -> int:
         nums.sort()
+        n = len(nums)
         
-        def check(num):
-            cnt = 0
-            i = 0
-            while i < len(nums) - 1:
-                if nums[i+1] - nums[i] <= mid: 
-                    cnt += 1
-                    i += 1
-                i+= 1
-            return cnt >= p
-     
-        l = 0
-        r = nums[-1] - nums[0]
-        while l < r:
-            mid = (l+r) // 2
-            if check(mid):
-                r = mid
-                
+        def countValidPairs(threshold):
+            index, count = 0, 0
+            while index < n - 1:
+                if nums[index + 1] - nums[index] <= threshold:
+                    count += 1
+                    index += 1
+                index += 1
+            return count
+        
+        left, right = 0, nums[-1] - nums[0]
+        while left < right:
+            mid = left + (right - left) // 2
+
+            if countValidPairs(mid) >= p:
+                right = mid
             else:
-                l = mid + 1
-        return l
-        
+                left = mid + 1
+        return left       
